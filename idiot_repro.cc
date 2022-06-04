df652e42a4a105e997dc5be6b46e03a4a6c81210
David Lehavi <dlehavi@resonai.com>
2022-06-04 23:02:51 +0300

    wip

--- idiot_repro.cc
+++ idiot_repro.cc
@@ -2,6 +2,6 @@
 using namespace std;
 
 int main (int argc, char* argv[]) {
-  system(("pause " + string(argv[1]) + string(argv[2])).c_str());
+  system(("repro " + string(argv[1]) + string(argv[2])).c_str());
   return 0;
 }
